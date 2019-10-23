00a49d07d58be059269fddd6f83ceba52d377b53
Mateusz Matyska <mateuszmatyska@gmail.com>
2019-10-23 17:05:01 +0200

    basic routing

---
+++ src/config/colors.tsx
@@ -0,0 +1,9 @@
+export const colors = {
+    primary: "#3340FF",
+    secondary: "#121DB3",
+    green: "#4FCC14",
+    darkGreen: "#4BB31B",
+    orange: "#FF834D",
+    black: "#000000",
+    white: "#ffffff",
+};
