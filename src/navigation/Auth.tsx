18d777ecd80a037853da36d1b4a5e45937d30a6e
Mateusz Matyska <mateuszmatyska@gmail.com>
2019-11-30 22:16:00 +0100

    Auth fix

--- src/navigation/Auth.tsx
+++ src/navigation/Auth.tsx
@@ -1,7 +1,13 @@
+import React from 'react';
+import {Redirect} from 'react-router-dom';
+
 export const Auth = () => {
     const user = localStorage.getItem('token');
 
     if(user) {
         localStorage.setItem('user', 'true');
+    }
+    else {
+        return <Redirect to="/login" />
     }
 }
