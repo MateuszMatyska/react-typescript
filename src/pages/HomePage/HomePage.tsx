03de8231cd1a306826e669e2dfb29f87ac759582
mateuszmatyska <mateuszmatyska@gmail.com>
2020-02-28 15:49:00 +0100

    last commit

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
-import UserComponent from 'components/UserComponent';
-import SetUserComponent from 'components/SetUserComponent';
 import {connect} from 'react-redux';
 import {IAppState} from 'store/Store';
+import { Link } from 'react-router-dom';
 
 const HomePage: React.FC<any> = (props: any) =>{    
     return <div>
         <div className="wrapper">
             <h1>Dashboard</h1>
-            <UserComponent />
-            <h1>----------</h1>
-            <SetUserComponent />
+            <span>
+                Web app allows you check weather edit and delete weather station measure.
+            </span>
+            <Link to='/weather'>Weather Site</Link>
         </div>
     </div>
 }
